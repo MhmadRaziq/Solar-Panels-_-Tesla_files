b3538f51adfb80fb2e5434bcebbf3bc52fbd2085
Mhmd Raziq <76057495+MhmadRaziq@users.noreply.github.com>
2024-11-05 12:25:00 +0200

    Add files via upload

---
+++ geo4.js
@@ -0,0 +1,17 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "NABLUS",
+      'continent': "AS",
+      'country': "PS",
+      'region': "NBS",
+      'dma': ""
+    },
+    'ip':"139.190.186.189"
+  }]);
+})
+
+()
+
+;
